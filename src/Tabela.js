d2026790f42e03378066d8559cb66394ba0f0899
Flavio Kenzo Nishiyama <f.kenzo.nishiyama@avanade.com>
2022-05-11 15:10:41 -0300

    Alterando a url da API e colocando formatações do css

--- src/Tabela.js
+++ src/Tabela.js
@@ -6,32 +6,55 @@ function Tabela(){
     const [usuarios, setUsuarios] = useState([]);
 
     useEffect(() => {
-        axios.get("https://iot.14mob.com/lista.json").then(response => {
-            setUsuarios(response.data);
+        axios.get("https://iot.14mob.com/api-fiap/public/index.php/users").then(response => {
+            setUsuarios(response.data.users);
             console.log(response);
         })
     },[])
 
     return(
-        <table className="minhaTabela">
-            <thead>
-                <tr>
-                    <th>ID</th>
-                    <th>Nome</th>
-                    <th>Email</th>
-                </tr>
-            </thead>
-            <tbody>
-                { usuarios.map( usuario => {
-                return <tr key={usuario.id} >
-                        <td>{usuario.id}</td>
-                        <td>{usuario.nome}</td>
-                        <td>{usuario.descricao}</td>
-                    </tr>
-                }   )   }
-            </tbody>
+        <div>
+            <form className='formulario'>
 
-        </table>
+                <div className="nome">
+                    <label>Nome:</label>
+                    <input name='name' value=''/>
+                </div>
+
+                <div className="email">
+                    <label>E-mail:</label>
+                    <input name='email' value=''/>
+                </div>
+
+                <div className='senha'>
+                    <label>Senha:</label>
+                    <input name='password' value=''/>
+                </div>
+
+                <button type='submit' className='botao'>Enviar</button>
+
+            </form>
+
+            <table className="minhaTabela">
+                <thead>
+                    <tr>
+                        <th>ID</th>
+                        <th>Nome</th>
+                        <th>Email</th>
+                    </tr>
+                </thead>
+                <tbody className='body'>
+                    { usuarios.map( usuario => {
+                    return <tr key={usuario.id} >
+                            <td>{usuario.id}</td>
+                            <td>{usuario.name}</td>
+                            <td>{usuario.email}</td>
+                        </tr>
+                    }   )   }
+                </tbody>
+
+            </table>
+        </div>
     );
 }
 

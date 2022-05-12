c788d22bf60be9b4d8a5664a3e0f5235ad6f7ea1
Flavio Kenzo Nishiyama <f.kenzo.nishiyama@avanade.com>
2022-05-12 11:26:28 -0300

    Criando chamada https com o método post

--- src/Tabela.js
+++ src/Tabela.js
@@ -5,6 +5,28 @@ function Tabela(){
 
     const [usuarios, setUsuarios] = useState([]);
 
+    const [nome, setNome] = useState("");
+    const [email, setEmail] = useState("");
+    const [senha, setSenha] = useState("");
+
+    function salvarFormulario(){
+
+        let parametros = {
+            name: nome,
+            email: email,
+            password: senha
+        }
+
+        axios.post('https://iot.14mob.com/api-fiap/public/index.php/users', parametros).then(response => {
+            if(response.status == 201){
+                alert('Deu certo');
+            }
+            else{
+                alert('Deu errado');
+            }
+        }).catch(error => console.log(error));
+    }
+
     useEffect(() => {
         axios.get("https://iot.14mob.com/api-fiap/public/index.php/users").then(response => {
             setUsuarios(response.data.users);
@@ -14,26 +36,34 @@ function Tabela(){
 
     return(
         <div>
-            <form className='formulario'>
+            <form className='formulario' onSubmit={e => {
+                e.preventDefault();
+                salvarFormulario();
+                return false;
+            }}>
 
                 <div className="nome">
                     <label>Nome:</label>
-                    <input name='name' value=''/>
+                    <input name='name' onChange={e => setNome(e.target.value)}/>
                 </div>
 
                 <div className="email">
                     <label>E-mail:</label>
-                    <input name='email' value=''/>
+                    <input name='email' onChange={e => setEmail(e.target.value)}/>
                 </div>
 
                 <div className='senha'>
                     <label>Senha:</label>
-                    <input name='password' value=''/>
+                    <input name='password' onChange={e => setSenha(e.target.value)}/>
                 </div>
 
                 <button type='submit' className='botao'>Enviar</button>
 
             </form>
+
+            <p>{nome}</p>
+            <p>{email}</p>
+            <p>{senha}</p>
 
             <table className="minhaTabela">
                 <thead>
